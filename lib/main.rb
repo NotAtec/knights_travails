9422fa720afe20b20f239586bac28705dfef8708
Jeroen B <jeroenb04@live.com>
2022-01-29 14:59:03 +0100

    Create first part of #build_tree
    For each possible move, #valid_move? is called to see if a move is valid
    
    #valid_move? returns false if a move would end up outside of the board

--- lib/main.rb
+++ lib/main.rb
@@ -25,9 +25,30 @@ class Moveset
     @root = Node.new(origin)
   end
 
-  def build_tree
+  def build_tree(position = @root)
+    return position unless position.exists?
+
+    MOVES.each do |move|
+      destination = [position[0] + move[0], position[1] + move[1]]
+      if valid_move?(destination)
+
+      end
+      # Check if move is valid
+      # Find node with new position after move
+      # If found: 
+      # Link found node to position
+      # If not found:
+      # Create new node
+      # Link new node to position
+      # Run tree method again for new node
+    end
+  end
+
+  def valid_move?(destination)
+    return false if destination[0].negative? || destination[0] > 7 || destination[1].negative? || destination[1] > 7
+
+    true
   end
 end
 
-def knight_moves(origin, destination)
-end
+def knight_moves(origin, destination); end
